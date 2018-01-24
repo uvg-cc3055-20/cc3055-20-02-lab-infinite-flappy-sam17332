b0d29dd725faa49b8f9374deae123c9230b9267a
sam17332 <32148906+sam17332@users.noreply.github.com>
2018-01-24 08:44:54 -0600

    lab2 terminado

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -18,4 +18,14 @@ public class BirdScript : MonoBehaviour {
         }
 	}
 
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		GameController.instance.score++;
+		Debug.Log (GameController.instance.score);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		GameController.instance.gameOver = true;
+	}
 }
---
+++ Assets/Scripts/ColumnSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnSpawner : MonoBehaviour {
+
+	public GameObject column;
+	public float spawnTime = 4f;
+	public float elapsedTime = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (GameController.instance.gameOver == false) {
+			if (elapsedTime < spawnTime) {
+				elapsedTime += Time.deltaTime;
+			} else {
+				float random = Random.Range (-2f, 2f);
+				Instantiate (column, new Vector3 (8, random, 0), Quaternion.identity);
+				elapsedTime = 0;
+			}
+		}
+	}
+}
